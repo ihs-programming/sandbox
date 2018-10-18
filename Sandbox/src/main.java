878ee73412724243794053cd68388f0363c7a38c
s-alapatin <s-alapatin@54L1809-25.it.bsd405.org>
2018-10-18 15:40:41 -0700

    println change

--- Sandbox/src/main.java
+++ Sandbox/src/main.java
@@ -2,7 +2,7 @@
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("whats up drama alert nation im ur host killer keem star lets get right into the news");
 	}
 
 }
